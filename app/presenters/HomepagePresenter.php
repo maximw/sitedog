37e0451c2bc6097b4810bec54c7e91c11ff7a177
Maxim Naguchev <me@maximw.com>
2015-07-23 18:30:08 +0300

    Add doctrine entity and wrapping model

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -3,8 +3,20 @@
 namespace App\Presenters;
 
 use Nette;
+use App\Models;
 
 
 class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+
+    /**
+     * @var \App\Models\Users @inject
+     */
+    public $usersModel;
+
+    public function actionDefault() {
+        $this->usersModel->create();
+        $this->usersModel->all();
+    }
+
 }
